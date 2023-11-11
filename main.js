3113a3b0ef127a05b5eef9ac3e7ca9fbe8362f3e
JUSTNPC404 <justnpc404@gmail.com>
2023-11-11 16:51:36 +0700

    initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+onload = () =>{
+    document.body.classList.remove("container");
+
+    document.addEventListener("DOMContentLoaded", function() {
+            var audio = document.getElementById("myAudio");
+            audio.play();
+        });
+};
